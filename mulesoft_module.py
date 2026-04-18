068ec081bc9ef3279913ccf397c8c312d10ee84e
devsha256 <dev.hsaddam@gmail.com>
2026-04-18 17:35:55 +0530

    Add dropdown option for anypoint platfor orgs & envs

--- mulesoft_module.py
+++ mulesoft_module.py
@@ -18,6 +18,30 @@ class MuleSoftManager:
             "Content-Type": "application/json"
         }
 
+    def get_organizations(self):
+        """Fetches all organizations the user has access to."""
+        url = f"{self.anypoint_url}/accounts/api/me"
+        try:
+            res = requests.get(url, headers=self.get_headers())
+            if res.status_code == 200:
+                data = res.json()
+                # Extracting the master org and any sub-organizations
+                user_obj = data.get('user', {})
+                member_groups = user_obj.get('memberOfOrganizations', [])
+                return member_groups
+            return []
+        except:
+            return []
+
+    def get_environments(self, org_id):
+        """Fetches environments for a specific organization ID."""
+        url = f"{self.anypoint_url}/accounts/api/organizations/{org_id}/environments"
+        try:
+            res = requests.get(url, headers=self.get_headers())
+            return res.json().get('data', []) if res.status_code == 200 else []
+        except:
+            return []
+
     def get_runtime_apps(self, org_id, env_id):
         url = f"{self.anypoint_url}/cloudhub/api/v2/applications"
         headers = self.get_headers()
